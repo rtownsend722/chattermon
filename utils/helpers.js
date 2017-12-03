67b5243d4e2e8ee134965e6887f5277c97fbcdf8
amboz <abozigian@gmail.com>
2017-12-03 13:16:52 -0800

    remove attack language from terminal help

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 const help = [
   {command: `Welcome to Chattermon. Chat your way to victory using these simple commands:`},
-  {command: `"attack" : Attacks with your current active pokemon.`},
-  {command: `"<move>" : Attacks with selected move.`},
+  // {command: `"attack" : Attacks with your current active pokemon.`},
+  {command: `"<move>" : Attack with your current pokemon and its selected move.`},
   {command: `"choose <pokemon>" : Swaps your current pokemon with a currently available pokemon.`},
   {command: `"flee": Run away from your opponent and forfeit the game.`},
   {command: `Have fun!`}
